8809ff200c91a13467209fcda3240a9a59be7b44
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-16 01:46:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts "#{season}:"
+    puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
       puts"  #{holiday.to_s.split('_').collect {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
     end
