0e607324ac7eddff046ccd2773c007a01a75f189
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-16 01:18:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def all_winter_holiday_supplies(holiday_hash)
       holiday_hash[:winter].collect do |key, value|
          return value
       end.flatten
+      binding.pry
 end
 
 def all_supplies_in_holidays(holiday_hash)
