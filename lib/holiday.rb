19f60083bad4bee45fc63cc6a82f0c59d6ba6f10
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-16 01:14:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
       holiday_hash[:winter].collect do |key, value|
-        puts value
+         return value
       end.flatten
 end
 
